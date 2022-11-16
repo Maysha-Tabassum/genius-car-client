8fe67e64dd804d72edf6d032ed8efefab1914c32
Maysha-Tabassum <mayshatabassum77@gmail.com>
2022-11-16 06:52:15 +0600

    login and signup added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,43 +1,45 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import img from '../../assets/images/login/login.svg';
 
 const Login = () => {
-const handleLogin = event => {
+  const handleLogin = event => {
     event.preventDefault();
-}
+  }
 
-    return (
-        <div className="hero w-full my-20">
-  <div className="hero-content flex-col grid gap-20 md:grid-cols-2">
-    <div className="text-center lg:text-left">
-      <img className='w-3/4' src={img} alt="" />
-    </div>
-    <form onSubmit={handleLogin} className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <h1 className="text-5xl font-bold text-center">Login</h1>
-      <div className="card-body">
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input type="text" placeholder="email" className="input input-bordered" />
+  return (
+    <div className="hero w-full my-20">
+      <div className="hero-content flex-col grid gap-20 md:grid-cols-2">
+        <div className="text-center lg:text-left">
+          <img className='w-3/4' src={img} alt="" />
         </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input type="text" placeholder="password" className="input input-bordered" />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
-        </div>
-        <div className="form-control mt-6">
-            <input className="btn btn-primary" type="submit" value="Login" />
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-20">
+          <h1 className="text-5xl font-bold text-center">Login</h1>
+          <form onSubmit={handleLogin} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input type="text" name='email' placeholder="email" className="input input-bordered" />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input type="text" name='password' placeholder="password" className="input input-bordered" />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <input className="btn bg-orange-600 border-none" type="submit" value="Login" />
+            </div>
+          </form>
+          <p className='text-center'>New to Genius Car ? <Link className='text-orange-600 font-bold' to="/signup">Sign Up</Link></p>
         </div>
       </div>
-    </form>
-  </div>
-</div>
-    );
+    </div>
+  );
 };
 
 export default Login;
